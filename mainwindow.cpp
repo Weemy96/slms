3407a91b79453e1b645fbe50fd986a0a9bb6db1c
Derek Wee <wee_my@hotmail.com>
2018-08-14 09:52:41 +0800

    edit markdown for bitbucket format

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QDateTime CurrentDateTime= QDateTime::currentDateTime();
+    ui->dateTimeEdit->setMinimumDateTime(CurrentDateTime);
 }
 
 MainWindow::~MainWindow()
