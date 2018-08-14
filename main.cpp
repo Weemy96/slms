48c4c09677e956e36cf3265909e95dffb1d32cf0
Derek Wee <wee_my@hotmail.com>
2018-08-14 09:52:41 +0800

    edit markdown for bitbucket format

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDir>
+#include <QFileInfo>
+#include <QFile>
 #include <QDesktopWidget>
+#include <QMessageBox>
+#include <QDebug>
+
+void CreateUser();
+void CreateRecord();
+void CreateAll();
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    bool isDataFolderExists = QDir("Data").exists();
+    if (!isDataFolderExists)
+    {
+        QDir().mkdir("Data");
+        CreateAll();
+    }
+    else
+    {
+        if(QDir("Data").isEmpty()) CreateAll();
+        if(QFileInfo("Data/user.txt").exists())
+        {
+            if(QFileInfo("Data/user.txt").isDir())
+            {
+                QDir("Data/user.txt").removeRecursively();
+                CreateUser();
+            }
+        }
+    }
+
     MainWindow w;
     //set application run center screen
     QDesktopWidget *scrSize = QApplication::desktop();
@@ -14,4 +42,22 @@ int main(int argc, char *argv[])
     w.show();
 
     return a.exec();
+}
+
+void CreateUser()
+{
+    QFile createFile("Data/user.txt");
+    createFile.open(QIODevice::WriteOnly|QIODevice::Text);
+    createFile.close();
+}
+void CreateRecord()
+{
+    QFile createFile("Data/record.txt");
+    createFile.open(QIODevice::WriteOnly|QIODevice::Text);
+    createFile.close();
+}
+void CreateAll()
+{
+    CreateUser();
+    CreateRecord();
 }
